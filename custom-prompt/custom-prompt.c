7f603a2a4814b5f78a086310446038b5bb077ca3
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-06-28 19:20:58 +0530

    Renamed `window_id` to `wid`.
    The description of `active_window_id` would not fit in one line in the documentation of `report_command_status` if it were renamed to `prev_active_window_id`, so I renamed it to `prev_active_wid`. Hence, I renamed all other identifiers as well.

--- custom-prompt/custom-prompt.c
+++ custom-prompt/custom-prompt.c
@@ -18,7 +18,7 @@ struct Interval
     unsigned milliseconds;
 };
 
-long long unsigned get_active_window_id(void);
+long long unsigned get_active_wid(void);
 
 #if defined __APPLE__
 #define HOST_ICON ""
@@ -223,11 +223,11 @@ void write_report(
  * @param last_command Most-recently run command.
  * @param exit_code Code with which the command exited.
  * @param delay Running time of the command in nanoseconds.
- * @param active_window_id ID of the focused window when the command started.
+ * @param prev_active_wid ID of the focused window when the command started.
  * @param columns Width of the terminal window.
  *****************************************************************************/
 void report_command_status(
-    char *last_command, int exit_code, long long unsigned delay, long long unsigned active_window_id, int columns)
+    char *last_command, int exit_code, long long unsigned delay, long long unsigned prev_active_wid, int columns)
 {
     LOG_DEBUG("Command '%s' exited with code %d in %llu ns.", last_command, exit_code, delay);
     if (delay <= 5000000000ULL)
@@ -256,10 +256,10 @@ void report_command_status(
     write_report(last_command, last_command_len, exit_code, &interval, columns);
     if (delay > 10000000000ULL)
     {
-        long long unsigned curr_active_window_id = get_active_window_id();
-        LOG_DEBUG("Active window ID when the command started was %llu.", active_window_id);
-        LOG_DEBUG("Active window ID when the command finished is %llu.", curr_active_window_id);
-        if (active_window_id != curr_active_window_id)
+        long long unsigned curr_active_wid = get_active_wid();
+        LOG_DEBUG("Active window ID when the command started was %llu.", prev_active_wid);
+        LOG_DEBUG("Active window ID when the command finished is %llu.", curr_active_wid);
+        if (prev_active_wid != curr_active_wid)
         {
             notify_desktop(last_command, &interval);
         }
@@ -312,7 +312,7 @@ int main(int const argc, char const *argv[])
     long long unsigned ts = get_timestamp();
     if (argc <= 1)
     {
-        printf("%llu %llu\n", ts, get_active_window_id());
+        printf("%llu %llu\n", ts, get_active_wid());
         return EXIT_SUCCESS;
     }
 
@@ -321,13 +321,13 @@ int main(int const argc, char const *argv[])
     char *last_command = (char *)argv[1];
     int exit_code = strtol(argv[2], NULL, 10);
     long long unsigned delay = ts - strtoll(argv[3], NULL, 10);
-    long long unsigned active_window_id = strtoull(argv[4], NULL, 10);
+    long long unsigned prev_active_wid = strtoull(argv[4], NULL, 10);
     int columns = strtol(argv[5], NULL, 10);
     char const *git_info = argv[6];
     int shlvl = strtol(argv[7], NULL, 10);
     char const *pwd = argv[8];
 
-    report_command_status(last_command, exit_code, delay, active_window_id, columns);
+    report_command_status(last_command, exit_code, delay, prev_active_wid, columns);
     display_primary_prompt(git_info, shlvl);
     update_terminal_title(pwd);
 
--- custom-prompt/get_active_window_id_windows.c
+++ custom-prompt/get_active_window_id_windows.c
@@ -1,7 +1,7 @@
 #ifdef _WIN32
 #include <windows.h>
 
-long long unsigned get_active_window_id(void)
+long long unsigned get_active_wid(void)
 {
     return (long long unsigned)GetForegroundWindow();
 }
