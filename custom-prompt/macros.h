2cad679e9a327a3ecbcb84a805b426ba16a2899d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-07-15 22:33:56 +0530

    Added forgotten file.

---
+++ custom-prompt/macros.h
@@ -0,0 +1,59 @@
+#ifndef TFPF_DOTFILES_CUSTOM_PROMPT_MACROS_H_
+#define TFPF_DOTFILES_CUSTOM_PROMPT_MACROS_H_
+
+#if defined __APPLE__
+#define HOST_ICON ""
+#elif defined __linux__
+#define HOST_ICON ""
+#elif defined _WIN32
+#define HOST_ICON ""
+#else
+#error "unknown OS"
+#endif
+
+#if defined BASH
+#define BEGIN_INVISIBLE "\x01"
+#define END_INVISIBLE "\x02"
+#define USER "\\u"
+#define HOST "\\h"
+#define DIRECTORY "\\w"
+#define PROMPT_SYMBOL "\\$"
+#elif defined ZSH
+#define BEGIN_INVISIBLE "%%\x7B"
+#define END_INVISIBLE "%%\x7D"
+#define USER "%%n"
+#define HOST "%%m"
+#define DIRECTORY "%%~"
+#define PROMPT_SYMBOL "%%#"
+#else
+#error "unknown shell"
+#endif
+
+#define ESCAPE "\x1B"
+#define LEFT_SQUARE_BRACKET "\x5B"
+#define BACKSLASH "\x5C"
+#define RIGHT_SQUARE_BRACKET "\x5D"
+
+// Bold, bright and italic.
+#define BBI_YELLOW BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "1;3;93m" END_INVISIBLE
+
+// Bold and bright.
+#define BB_CYAN BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "1;96m" END_INVISIBLE
+#define BB_GREEN BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "1;92m" END_INVISIBLE
+
+// Bright.
+#define B_BLUE BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "94m" END_INVISIBLE
+#define B_GREEN_RAW ESCAPE LEFT_SQUARE_BRACKET "92m"
+#define B_GREY_RAW ESCAPE LEFT_SQUARE_BRACKET "90m"
+#define B_RED_RAW ESCAPE LEFT_SQUARE_BRACKET "91m"
+
+// Dark.
+#define D_CYAN_RAW ESCAPE LEFT_SQUARE_BRACKET "36m"
+#define D_GREEN BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "32m" END_INVISIBLE
+#define D_RED BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "31m" END_INVISIBLE
+
+// No formatting.
+#define RESET BEGIN_INVISIBLE ESCAPE LEFT_SQUARE_BRACKET "m" END_INVISIBLE
+#define RESET_RAW ESCAPE LEFT_SQUARE_BRACKET "m"
+
+#endif
