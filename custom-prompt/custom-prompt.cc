4c4833f7a7e0c48cd183b0b2a52edcfad7d75998
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-07-21 23:25:49 +0530

    Moved `interval` calculation after length log.

--- custom-prompt/custom-prompt.cc
+++ custom-prompt/custom-prompt.cc
@@ -237,14 +237,13 @@ void report_command_status(std::string_view& last_command, int exit_code, long l
 #endif
     }
 
-    Interval interval = delay_to_interval(delay);
-
 #ifdef BASH
     // Remove the initial part (index and timestamp) of the command.
     last_command = last_command.substr(last_command.find(RIGHT_SQUARE_BRACKET[0]) + 2);
 #endif
     last_command.remove_suffix(last_command.size() - 1 - last_command.find_last_not_of(' '));
     LOG_DEBUG("Command length is %zu.", last_command.size());
+    Interval interval = delay_to_interval(delay);
 
     write_report(last_command, exit_code, interval, columns);
     if (delay > 10000000000ULL)
