692ff0f359e833bf07ab6a4e784fc860b5f03874
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-08-16 10:47:31 +0530

    Linux: do not crash if no display server is running

--- custom-prompt/get_active_wid_linux.c
+++ custom-prompt/get_active_wid_linux.c
@@ -6,6 +6,8 @@
 long long unsigned get_active_wid(void)
 {
     Display* display = XOpenDisplay(NULL);
+    if(display==NULL){return 0;}
+
     Window root_window = DefaultRootWindow(display);
     Atom property = XInternAtom(display, "_NET_ACTIVE_WINDOW", False);
 
