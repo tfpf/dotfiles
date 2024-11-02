f09afb0c700fd653fbb736968e561876aa2c32a4
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-11-02 23:07:55 +0530

    Ternary operator to check timeout

--- custom-prompt/custom-prompt.cc
+++ custom-prompt/custom-prompt.cc
@@ -681,19 +681,13 @@ int main_internal(int const argc, char const* argv[])
     report_command_status(last_command, exit_code, delay, prev_active_wid, columns);
 
     int shlvl = std::stoi(argv[6]);
-    if (git_repository_information_future.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready)
-    {
-        display_primary_prompt(shlvl, git_repository_information_future.get());
-    }
-    else
-    {
-        display_primary_prompt(shlvl, "unknown");
-    }
+        display_primary_prompt(shlvl, git_repository_information_future.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready?git_repository_information_future.get(): "unavailable");
 
     std::string_view pwd(argv[7]);
     set_terminal_title(pwd);
 
-    // Exit without waiting for the other thread to terminate.
+    // Exit without waiting for the other thread to terminate. Should not be a
+    // problem, because that thread has no side-effects that I know of.
     std::exit(EXIT_SUCCESS);
 }
 
