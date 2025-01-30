815f681174d8f9019bab1112653176b2ba84db46
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-01-30 20:27:49 +0530

    Make Kitty behave more consistently for my usage (#78)

---
+++ .config/kitty/tab_bar.py
@@ -0,0 +1,14 @@
+import platform
+
+from kitty.tab_bar import DrawData, draw_tab_with_powerline
+
+_modifier_key = "⌘" if platform.system() == "Darwin" else "⎇"
+_title_template = (
+    "{fmt.fg.red}{activity_symbol or ' '}{fmt.fg.darkmagenta}" + _modifier_key + " {index}{fmt.fg.tab} {title}"
+)
+
+
+def draw_tab(draw_data: DrawData, *args) -> int:
+    # The private-looking method of a named tuple I call here is documented, so
+    # I assume it is not actually private.
+    return draw_tab_with_powerline(draw_data._replace(title_template=_title_template), *args)
