40415dc9d215a838b4d98922accabed1ec1898e3
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-12 21:56:29 +0530

    Put code into `class`

--- diff/diff.py
+++ diff/diff.py
@@ -5,22 +5,31 @@ import os
 import sys
 
 
-def report(directory_comparison: filecmp.dircmp, path_prefix: str = ""):
+class Diff:
     """
-    Write an HTML snippet summarising the differences recorded between two
-    directories. Then recurse on their subdirectories.
-    :param directory_comparison: Directory comparison object.
-    :param path_prefix: Common part of the path to the directories.
+    Compare two directories recursively.
     """
-    print(path_prefix, directory_comparison.left_only, directory_comparison.right_only,
-          directory_comparison.diff_files)
-    for subdirectory, subdirectory_comparison in directory_comparison.subdirs.items():
-        report(subdirectory_comparison, os.path.join(path_prefix, subdirectory))
+
+    def __init__(self, a: str, b: str):
+        self._directory_comparison = filecmp.dircmp(a, b, shallow=True)
+
+    def report(self, directory_comparison: filecmp.dircmp | None = None, path_prefix: str = ""):
+        """
+        Write an HTML snippet summarising the differences recorded between two
+        directories. Then recurse on their subdirectories.
+        :param directory_comparison: Directory comparison object.
+        :param path_prefix: Common part of the path to the directories.
+        """
+        directory_comparison = directory_comparison or self._directory_comparison
+        print(path_prefix, directory_comparison.left_only, directory_comparison.right_only,
+              directory_comparison.diff_files)
+        for subdirectory, subdirectory_comparison in directory_comparison.subdirs.items():
+            self.report(subdirectory_comparison, os.path.join(path_prefix, subdirectory))
 
 
 def main():
-    directory_comparison = filecmp.dircmp(sys.argv[1], sys.argv[2], shallow=False)
-    report(directory_comparison)
+    diff = Diff(sys.argv[1], sys.argv[2])
+    diff.report()
 
 
 if __name__ == "__main__":
