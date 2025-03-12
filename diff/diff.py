75d63d9db2b9f32b37af89239ef126e61e4c73d6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-12 21:35:53 +0530

    Skeleton code for iterating over differences

--- diff/diff.py
+++ diff/diff.py
@@ -1,18 +1,26 @@
 #! /usr/bin/env python
 
 import filecmp
-import os.path
+import os
 import sys
 
 
-def report(dc: filecmp.dircmp, path):
-    print(path, dc.left_only, dc.right_only, dc.diff_files)
-    for subdir, subdc in dc.subdirs.items():
-        report(subdc, os.path.join(path, subdir))
+def report(directory_comparison: filecmp.dircmp, path_prefix: str = ""):
+    """
+    Write an HTML snippet summarising the differences recorded between two
+    directories. Then recurse on their subdirectories.
+    :param directory_comparison: Directory comparison object.
+    :param path_prefix: Common part of the path_prefix to the directories.
+    """
+    print(path_prefix, directory_comparison.left_only, directory_comparison.right_only,
+          directory_comparison.diff_files)
+    for subdirectory, subdirectory_comparison in directory_comparison.subdirs.items():
+        report(subdirectory_comparison, os.path.join(path_prefix, subdirectory))
+
 
 def main():
-    dc = filecmp.dircmp(sys.argv[1], sys.argv[2], shallow=False)
-    report(dc, "")
+    directory_comparison = filecmp.dircmp(sys.argv[1], sys.argv[2], shallow=False)
+    report(directory_comparison)
 
 
 if __name__ == "__main__":
