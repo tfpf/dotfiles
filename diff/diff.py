188c79b6587a1efcbd9ee4a0de1eed4b17d411d9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-12 21:17:25 +0530

    Recursive report code

--- diff/diff.py
+++ diff/diff.py
@@ -1,1 +1,19 @@
 #! /usr/bin/env python
+
+import filecmp
+import os.path
+import sys
+
+
+def report(dc: filecmp.dircmp, path):
+    print(path, dc.left_only, dc.right_only, dc.diff_files)
+    for subdir, subdc in dc.subdirs.items():
+        report(subdc, os.path.join(path, subdir))
+
+def main():
+    dc = filecmp.dircmp(sys.argv[1], sys.argv[2], shallow=False)
+    report(dc, "")
+
+
+if __name__ == "__main__":
+    main()
