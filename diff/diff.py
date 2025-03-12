a8c7aab219ddf56d150d9ee637633459a6a352a7
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-12 22:14:58 +0530

    Correct recursion on subdirectories

--- diff/diff.py
+++ diff/diff.py
@@ -11,25 +11,33 @@ class Diff:
     """
 
     def __init__(self, a: str, b: str):
-        self._directory_comparison = filecmp.dircmp(a, b, shallow=True)
+        self._directory_comparison = filecmp.dircmp(a, b, shallow=False)
 
-    def report(self, directory_comparison: filecmp.dircmp | None = None, common_path: str = ""):
+    def _report(self, from_file: str | None, to_file: str | None):
+        print(from_file, to_file)
+
+    def report(self, directory_comparison: filecmp.dircmp | None = None):
         """
         Write an HTML table summarising the differences recorded between two
         directories. Then recurse on their subdirectories.
         :param directory_comparison: Directory comparison object.
-        :param common_path: Common part of the path to the directories.
         """
         directory_comparison = directory_comparison or self._directory_comparison
-        print(common_path, directory_comparison.left_only, directory_comparison.right_only,
-              directory_comparison.diff_files)
-        for subdirectory, subdirectory_comparison in directory_comparison.subdirs.items():
-            self.report(subdirectory_comparison, os.path.join(common_path, subdirectory))
+        for deleted_file in directory_comparison.left_only:
+            self._report(os.path.join(directory_comparison.left, deleted_file), None)
+        for added_file in directory_comparison.right_only:
+            self._report(None, os.path.join(directory_comparison.right, added_file))
+        for changed_file in directory_comparison.diff_files:
+            self._report(os.path.join(directory_comparison.left, changed_file),
+                         os.path.join(directory_comparison.right, changed_file))
+        for subdirectory_comparison in directory_comparison.subdirs.values():
+            self.report(subdirectory_comparison)
 
 
 def main():
     diff = Diff(sys.argv[1], sys.argv[2])
     diff.report()
+    input()
 
 
 if __name__ == "__main__":
