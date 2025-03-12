d48f17d03737aef81b67627500189b64f6374aa2
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-12 22:45:35 +0530

    Writing report to temporary file

--- diff/diff.py
+++ diff/diff.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
-
+import difflib
 import filecmp
 import os
 import sys
 import tempfile
-import typing
 import webbrowser
 
 
@@ -13,12 +12,13 @@ class Diff:
     Compare two directories recursively. Don't attempt to detect renames.
     """
 
-    def __init__(self, a: str, b: str, writer: typing.IO):
+    def __init__(self, a: str, b: str, writer):
         self._directory_comparison = filecmp.dircmp(a, b, shallow=False)
         self._writer = writer
+        self._html_diff = difflib.HtmlDiff()
 
-    def _report(self, from_file: str | None, to_file: str | None):
-        print(from_file, to_file)
+    def _report(self, fromlines: list[str], tolines: list[str]):
+        print(self._html_diff.make_table(fromlines, tolines).encode(), file=self._writer)
 
     def report(self, directory_comparison: filecmp.dircmp | None = None):
         """
@@ -28,12 +28,17 @@ class Diff:
         """
         directory_comparison = directory_comparison or self._directory_comparison
         for deleted_file in directory_comparison.left_only:
-            self._report(os.path.join(directory_comparison.left, deleted_file), None)
+            with open(os.path.join(directory_comparison.left, deleted_file)) as deleted_reader:
+                self._report(deleted_reader.readlines(), [])
         for added_file in directory_comparison.right_only:
-            self._report(None, os.path.join(directory_comparison.right, added_file))
+            with open(os.path.join(directory_comparison.right, added_file)) as added_reader:
+                self._report([], added_reader.readlines())
         for changed_file in directory_comparison.diff_files:
-            self._report(os.path.join(directory_comparison.left, changed_file),
-                         os.path.join(directory_comparison.right, changed_file))
+            with (
+                open(os.path.join(directory_comparison.left, changed_file)) as deleted_reader,
+                open(os.path.join(directory_comparison.right, changed_file)) as added_reader,
+            ):
+                self._report(deleted_reader.readlines(), added_reader.readlines())
         for subdirectory_comparison in directory_comparison.subdirs.values():
             self.report(subdirectory_comparison)
 
