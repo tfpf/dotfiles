655d37ff6835b02b02bb90f10198cf1d08bdb31a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-04-15 19:51:28 +0530

    Removed unnecessary shell script

--- diff/diff.py.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-# The external diff tool relies on the built-in hash function in Python being
-# deterministic across interpreter sessions. (Hashes of files are calculated in
-# difference processes.) Hence, disable hash randomisation.
-export PYTHONHASHSEED=0
