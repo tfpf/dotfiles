48b3a904c02de01b727790a091d0cf5e1a8efbdb
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-02 00:53:00 +0530

    IPython configuration file (#130)

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,24 @@
+import platform
+import sys
+
+import IPython
+
+c = get_config()  # noqa: F821
+c.InteractiveShellApp.exec_PYTHONSTARTUP = True
+c.InteractiveShellApp.ignore_cwd = False
+c.InteractiveShellApp.pylab_import_all = False
+c.BaseIPythonApplication.profile = "default"
+c.TerminalIPythonApp.display_banner = True
+c.InteractiveShell.autocall = 0
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.automagic = True
+c.InteractiveShell.banner1 = f"{platform.python_implementation()} {sys.version} with IPython {IPython.__version__}\n"
+c.InteractiveShell.color_info = True
+c.InteractiveShell.colors = "Linux"
+c.TerminalInteractiveShell.auto_match = False
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.display_completions = "readlinelike"
+c.TerminalInteractiveShell.highlight_matching_brackets = True
+c.TerminalInteractiveShell.highlighting_style = "gruvbox-dark"
+c.TerminalInteractiveShell.mouse_support = False
+c.Completer.backslash_combining_completions = True
