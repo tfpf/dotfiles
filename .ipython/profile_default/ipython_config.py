6c01f6f9597387d5b69e6fcc9eee30135e0fe7c9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-09 00:28:54 +0530

    Update IPython configuration for 8.5 and 9.0 both

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -13,12 +13,16 @@ c.InteractiveShell.autocall = 0
 c.InteractiveShell.autoindent = True
 c.InteractiveShell.automagic = True
 c.InteractiveShell.banner1 = f"{platform.python_implementation()} {sys.version} with IPython {IPython.__version__}\n"
-c.InteractiveShell.color_info = True
 c.InteractiveShell.colors = "Linux"
 c.TerminalInteractiveShell.auto_match = False
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.display_completions = "readlinelike"
 c.TerminalInteractiveShell.highlight_matching_brackets = True
-c.TerminalInteractiveShell.highlighting_style = "gruvbox-dark"
 c.TerminalInteractiveShell.mouse_support = False
 c.Completer.backslash_combining_completions = True
+
+if IPython.version_info < (9,):
+    c.TerminalInteractiveShell.highlighting_style = "gruvbox-dark"
+if IPython.version_info >= (9,):
+    c.InteractiveShell.colors = "neutral"
+    c.InteractiveShell.enable_tip = False
