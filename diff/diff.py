1d1d1b09df6b10357228d1c62272cbb3c02d8360
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-07-29 00:11:46 +0530

    File diff: preserve arguments in headers
    Unless one path is absolute and the other is relative. In that case, show both paths up to but excluding their common prefix.

--- diff/diff.py
+++ diff/diff.py
@@ -86,11 +86,12 @@ class Diff:
     """
 
     def __init__(self, left: str, right: str):
-        self._left_directory, self._right_directory = Path(left), Path(right)
+        self._left_directory, self._right_directory = Path(left).absolute(), Path(right).absolute()
         if self._left_directory.is_file() and self._right_directory.is_file():
             self._left_right_file_mapping = {self._left_directory: self._right_directory}
             self._left_files, self._right_files = set(), set()
-            self._left_directory, self._right_directory = self._left_directory.parent, self._right_directory.parent
+            self._left_directory = Path(os.path.commonprefix([self._left_directory, self._right_directory]))
+            self._right_directory = self._left_directory
         elif self._left_directory.is_dir() and self._right_directory.is_dir():
             self._matcher = difflib.SequenceMatcher(isjunk=None, autojunk=False)
             self._left_right_file_mapping = (
