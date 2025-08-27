b57ac344b39da8e00dd426661c4bb64a8136449d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-08-27 11:48:49 +0530

    Integrate JSON logging [skip ci]

--- custom-prompt/json_logger.hh
+++ custom-prompt/json_logger.hh
@@ -1,19 +1,21 @@
 #ifndef JSON_LOGGER_HH_
 #define JSON_LOGGER_HH_
 
-#include <variant>
+#include <cstdint>
 #include <string_view>
 #include <utility>
-#include <cstdint>
+#include <variant>
 #include <vector>
 
 using JSONKey = char const*;
 using JSONValue = std::variant<std::intmax_t, std::string_view>;
 
-void log_debug(char const* , char const* , std::intmax_t , char const *msg, std::vector<std::pair<JSONKey, JSONValue>> = {});
+void log_debug(
+    char const*, char const*, std::intmax_t, char const* msg, std::vector<std::pair<JSONKey, JSONValue>> = {}
+);
 
 #ifndef NDEBUG
-#define LOG_DEBUG(...) log_debug(__FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+#define LOG_DEBUG(...) log_debug(__FILE__, __func__, __LINE__ __VA_OPT__(, ) __VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
 #endif
