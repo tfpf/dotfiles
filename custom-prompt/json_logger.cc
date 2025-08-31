1a7fd172d09392da9c52c5d8afe8d5215aefa606
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-08-31 18:43:04 +0530

    Escape special characters in JSON strings (#146)
    I should probably extract out the JSON printing code into a separate library.🥱

--- custom-prompt/json_logger.cc
+++ custom-prompt/json_logger.cc
@@ -12,41 +12,86 @@
 #define LEFT_CURLY_BRACKET "\x7B"
 #define RIGHT_CURLY_BRACKET "\x7D"
 
-void log_debug(
+JSONString::JSONString(char const* s) : sv(s)
+{
+}
+
+JSONString::JSONString(std::string const& s) : sv(s)
+{
+}
+
+JSONString::JSONString(std::string_view const& s) : sv(s)
+{
+}
+
+JSONString::JSONString(JSONString const& s) : sv(s.sv)
+{
+}
+
+std::ostream& operator<<(std::ostream& ostream, JSONString const& json_string)
+{
+    ostream << '"';
+    for (auto const& c : json_string.sv)
+    {
+        switch (c)
+        {
+        case '\t':
+            ostream << "\\t";
+            break;
+        case '\n':
+            ostream << "\\n";
+            break;
+        case '\v':
+            ostream << "\\v";
+            break;
+        case '"':
+            ostream << "\\\"";
+            break;
+        default:
+            if (std::isprint(c))
+            {
+                ostream << c;
+            }
+            else
+            {
+                ostream << "\\x" << static_cast<int>(c);
+            }
+        }
+    }
+    ostream << '"';
+    return ostream;
+}
+
+void JSONLogger::log_debug(
     char const* file, char const* func, std::uintmax_t line, char const* msg,
     std::vector<std::pair<JSONKey, JSONValue>> msg_args
 )
 {
-    std::ostringstream oss;
     std::time_t curr_time = std::time(nullptr);
     char curr_time_buf[32];
     std::strftime(curr_time_buf, sizeof curr_time_buf / sizeof *curr_time_buf, "%FT%T%z", std::localtime(&curr_time));
-    oss << LEFT_CURLY_BRACKET "\"created\":\"" << curr_time_buf << "\"";
-    oss << ",\"source\":" LEFT_CURLY_BRACKET "\"file\":\"" << file << "\",\"func\":\"" << func
-        << "\",\"line\":" << line << RIGHT_CURLY_BRACKET;
-    oss << ",\"msg\":\"" << msg << "\"";
+
+    std::ostringstream oss;
+    oss << LEFT_CURLY_BRACKET << JSONString("created") << ":" << JSONString(curr_time_buf);
+    oss << ',' << JSONString("source") << ":" LEFT_CURLY_BRACKET << JSONString("file") << ":" << JSONString(file)
+        << ',' << JSONString("func") << ":" << JSONString(func) << ',' << JSONString("line") << ":" << line
+        << RIGHT_CURLY_BRACKET;
+    oss << ',' << JSONString("msg") << ":" << JSONString(msg);
 
     if (!msg_args.empty())
     {
-        oss << ",\"msg_args\":" LEFT_CURLY_BRACKET;
+        oss << ',' << JSONString("msg_args") << ":" << LEFT_CURLY_BRACKET;
         char const* delimiter = "";
         char const* actual_delimiter = ",";
         for (auto const& msg_arg : msg_args)
         {
-            if (auto val = std::get_if<std::string_view>(&msg_arg.second))
-            {
-                oss << delimiter << "\"" << msg_arg.first << "\":\"" << *val << "\"";
-            }
-            else
-            {
-                std::visit(
-                    [&](auto val)
-                    {
-                        oss << delimiter << "\"" << msg_arg.first << "\":" << val;
-                    },
-                    msg_arg.second
-                );
-            }
+            std::visit(
+                [&](auto const& val)
+                {
+                    oss << delimiter << msg_arg.first << ":" << val;
+                },
+                msg_arg.second
+            );
             delimiter = actual_delimiter;
         }
         oss << RIGHT_CURLY_BRACKET;
--- custom-prompt/json_logger.hh
+++ custom-prompt/json_logger.hh
@@ -2,20 +2,39 @@
 #define JSON_LOGGER_HH_
 
 #include <cstdint>
+#include <iostream>
 #include <string_view>
 #include <utility>
 #include <variant>
 #include <vector>
 
-using JSONKey = char const*;
-using JSONValue = std::variant<int, long, long long, long long unsigned, long unsigned, unsigned, std::string_view>;
+class JSONString
+{
+private:
+    std::string_view sv;
 
-void log_debug(
-    char const*, char const*, std::uintmax_t, char const* msg, std::vector<std::pair<JSONKey, JSONValue>> = {}
-);
+public:
+    JSONString(char const*);
+    JSONString(std::string const&);
+    JSONString(std::string_view const&);
+    JSONString(JSONString const&);
+
+    friend std::ostream& operator<<(std::ostream&, JSONString const&);
+};
+
+using JSONKey = JSONString;
+using JSONValue = std::variant<int, long, long long, long long unsigned, long unsigned, unsigned, JSONString>;
+
+class JSONLogger
+{
+public:
+    void log_debug(
+        char const*, char const*, std::uintmax_t, char const* msg, std::vector<std::pair<JSONKey, JSONValue>> = {}
+    );
+};
 
 #ifndef NDEBUG
-#define LOG_DEBUG(...) log_debug(__FILE__, __func__, __LINE__ __VA_OPT__(, ) __VA_ARGS__)
+#define LOG_DEBUG(logger, ...) logger.log_debug(__FILE__, __func__, __LINE__ __VA_OPT__(, ) __VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
 #endif
