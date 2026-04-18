60d0b81f996430505d71559875a756fda175177b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-04-19 01:05:53 +0530

    Simplify declaration of `get_active_wid`

--- custom-prompt/get_active_wid.hh
+++ custom-prompt/get_active_wid.hh
@@ -1,21 +1,19 @@
 #ifndef GET_ACTIVE_WID_HH_
 #define GET_ACTIVE_WID_HH_
 
-#ifdef __APPLE__
-/*
- * Get the ID of the currently-focused window.
- *
- * @return Active window ID. If no topmost window is found, 0 is returned.
- */
-extern "C" long long unsigned get_active_wid(void);
-#else
-/*
- * Get the ID of the currently-focused window.
- *
- * @return Active window ID. On Linux, if there is no X display running, 0 is
- * returned.
- */
-long long unsigned get_active_wid(void);
+#ifdef __cplusplus
+extern "C" {
 #endif
 
+/*
+ * Get the ID of the currently-focused window.
+ *
+ * On Linux, this always returns 0.
+ *
+ * @return Active window ID.
+ */
+long long unsigned get_active_wid(void);
+
+#ifdef __cplusplus
+}
 #endif
