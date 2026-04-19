d0f0298532e88b80037fa8c9c35ca5a3875a43f6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-04-19 22:42:30 +0530

    Add automatic terminal setting changing logic

--- custom-prompt/focus_utils.cc
+++ custom-prompt/focus_utils.cc
@@ -1,7 +1,8 @@
 #ifdef _WIN32
 
-#include "focus_utils.hh"
 #include <windows.h>
+
+#include "focus_utils.hh"
 
 long long unsigned get_active_wid(void)
 {
@@ -15,18 +16,64 @@ bool terminal_has_focus(void)
 
 #else  ////////////////////////////////////////////////////////////////////////
 
-#include <iostream>
-
+#include <stddef.h>
 #include <termios.h>
 #include <unistd.h>
+
+#include "focus_utils.hh"
 
 long long unsigned get_active_wid(void)
 {
     return 0;
 }
 
+class NonBlockingStandardInputReader
+{
+private:
+    bool error_occurred;
+    termios prev_termios, curr_termios;
+
+public:
+    NonBlockingStandardInputReader(void);
+    ssize_t read(char[], size_t);
+    ~NonBlockingStandardInputReader();
+};
+
+NonBlockingStandardInputReader::NonBlockingStandardInputReader(void) : error_occurred(false)
+{
+    if (tcgetattr(STDIN_FILENO, &this->prev_termios) == -1)
+    {
+        this->error_occurred = true;
+    }
+    this->curr_termios = this->prev_termios;
+    this->curr_termios.c_lflag &= ~(ECHO | ICANON);
+    this->curr_termios.c_cc[VMIN] = 0;
+    this->curr_termios.c_cc[VTIME] = 0;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &this->curr_termios) == -1)
+    {
+        this->error_occurred = true;
+    }
+}
+
+ssize_t NonBlockingStandardInputReader::read(char buf[], size_t count)
+{
+    return ::read(STDIN_FILENO, buf, count);
+}
+
+NonBlockingStandardInputReader::~NonBlockingStandardInputReader()
+{
+    if (this->error_occurred)
+    {
+        return;
+    }
+    tcsetattr(STDIN_FILENO, TCSANOW, &this->prev_termios);
+}
+
 bool terminal_has_focus(void)
 {
+    char buf[1024] = {};
+    ssize_t cnt = NonBlockingStandardInputReader().read(buf, sizeof buf / sizeof *buf);
+
     return false;
 }
 
