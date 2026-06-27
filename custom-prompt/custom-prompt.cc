87c5c8bb1d2c193d7426f2cf440fe4dd269f59c0
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-06-28 00:41:17 +0530

    Forgot to change test [skip ci]

--- custom-prompt/custom-prompt.cc
+++ custom-prompt/custom-prompt.cc
@@ -796,7 +796,7 @@ int main(int const argc, char const* argv[])
     // null-terminated.
     if (argc == 2)
     {
-        char const* argv[] = { "custom-prompt", "[] last_command", "0", "0", "79", "/", "1", nullptr };
+        char const* argv[] = { "custom-prompt", "[] last_command", "0", "0", "3661.001", "79", "/", "1", nullptr };
         int constexpr argc = sizeof argv / sizeof *argv - 1;
         return main_internal(argc, argv);
     }
